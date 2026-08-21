3da79783100392451bdddc431ea58755758047e6
liang.deng <liang.deng@mapabc.com>
2026-08-21 17:29:34 +0800

    add install comment

--- camera_sign_measure/depth_anything.py
+++ camera_sign_measure/depth_anything.py
@@ -1,6 +1,9 @@
 import torch
 from depth_anything_3.api import DepthAnything3
 
+# $env:HTTP_PROXY="http://172.20.10.2:18080"
+# $env:HTTPS_PROXY="http://172.20.10.2:18080"
+# pip install "git+https://github.com/ByteDance-Seed/Depth-Anything-3.git"
 
 class DepthAnythingV3:
 
